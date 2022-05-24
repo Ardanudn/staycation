e845b2981e755017527e6191197c43c21c9426ea
Ardanudn <63992156+Ardanudn@users.noreply.github.com>
2022-05-24 10:58:02 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Checkout from "pages/Checkout";
+
+import 'assets/scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route path="/checkout" component={Checkout} />
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
